bf046b140230877dc8cd28ab56fa1c24abb62244
Gideon-wang <gideonw.25225@obhs.school.nz>
2025-08-15 10:20:44 +1200

    add condition to loop and input

--- adventure.py
+++ adventure.py
@@ -1,2 +1,11 @@
-while True:
-    letter=input("enter a letter:")
+hp=100#our hitpoints
+dff=13#our defence
+en_hp=100#enemy hitpoints
+en_dff=13#enemy defence
+while hp>0 and en_hp>0:#checking if both alive
+    letter=input('''
+        enter a attack:
+        [s]word
+        [a]xe
+        [h]eal
+        ''')#enter our attack
