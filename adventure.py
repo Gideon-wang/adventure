6e14ace060b98c6b94e5b2970cc10f20a78575c0
Gideon-wang <gideonw.25225@obhs.school.nz>
2025-08-15 10:24:15 +1200

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -3,9 +3,15 @@ dff=13#our defence
 en_hp=100#enemy hitpoints
 en_dff=13#enemy defence
 while hp>0 and en_hp>0:#checking if both alive
-    letter=input('''
+    attack=input('''
         enter a attack:
         [s]word
         [a]xe
         [h]eal
         ''')#enter our attack
+    if attack=='s':
+        pass
+    elif attack=='a':
+        pass
+    elif attack=='h':
+        pass
